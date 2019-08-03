f4458357a085b3366b7998a55223b6d7162394af
smenaaliaga <smenaaliaga@hotmail.com>
2019-08-02 20:36:56 -0400

    Se añade funcionalidad de editar categorias

--- IoTMarket/Areas/Admin/Controllers/CategoryController.cs
+++ IoTMarket/Areas/Admin/Controllers/CategoryController.cs
@@ -45,5 +45,34 @@ namespace IoTMarket.Areas.Admin.Controllers
             }
             return View(category);
         }
+
+        // GET - EDIT
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(category);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
     }
 }
